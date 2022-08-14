57f4a0edffcf2b7ea59e6bd2d49d51b6b9adbfcf
Ryan Santos <rsantos@weareenvoy.com>
2022-08-13 23:06:55 -0700

    cleanup and refactor

--- src/App.js
+++ src/App.js
@@ -1,97 +1,109 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
-import './App.css';
+
 import CodeImage from './code-image.png';
 import pens from './data';
+import './App.css';
 
-const cn = (entries) =>
+const cn = entries =>
   Object.entries(entries)
-    .filter((entry) => entry[1])
-    .map((entry) => entry[0])
+    .filter(entry => entry[1])
+    .map(entry => entry[0])
     .join(' ');
 
-function IFrame ({ title, id, classNames }) {
+function IFrame ({ title, id, className }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <li className={cn({ 'pen-item': true, ...classNames })}>
-      <div className="pen-item-title">{title}</div>
+    <div className={cn({ 'pen-item': true, [className]: true })}>
+      <div className='title'>{title}</div>
+      {isLoading && (
+        <div className="loader" />
+      )}
       <iframe
-        loading="lazy"
-        scrolling="no"
-        frameBorder="no"
+        loading='lazy'
+        scrolling='no'
+        frameBorder='no'
+        allowtransparency='true'
         allowFullScreen={true}
-        allowtransparency="true"
-        className="pen-item-iframe"
         title={`100 Days CSS - ${ title }`}
-        src={`https://codepen.io/ryasan86/embed/${ id }?default-tab=result&theme-id=dark`}>
-        See the Pen <a href={`https://codepen.io/ryasan86/pen/${ id }`}>100 Days CSS - 001 Title</a>
-        by Ryan Santos (<a href="https://codepen.io/ryasan86">@ryasan86</a>) on
-        <a href="https://codepen.io">CodePen</a>.
+        src={`https://codepen.io/ryasan86/embed/${ id }?default-tab=result&theme-id=dark`}
+        onLoad={() => setIsLoading(false)}
+      >
+        See the Pen{' '}
+        <a href={`https://codepen.io/ryasan86/pen/${ id }`}>
+          100 Days CSS - {title}
+        </a>
+        by Ryan Santos (<a href='https://codepen.io/ryasan86'>@ryasan86</a>) on
+        <a href='https://codepen.io'>
+          CodePen
+        </a>.
       </iframe>
-    </li>
+    </div>
   );
 }
-
-IFrame.propTypes = {
-  title: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  classNames: PropTypes.object,
-};
 
 function App () {
   const [activePen, setActivePen] = useState(null);
 
   return (
-    <div className="app">
-      <div className="inner">
-        <header className="header">
-          <div className="header-column">
-            <h1 className="header-title">100 Days of <br /> CSS</h1>
-            <p className="header-subtitle">
+    <div className='app'>
+      <div className='inner'>
+        <header className='header'>
+          <div className='hero'>
+            <h1 className='title'>
+              100 Days of <br /> CSS
+            </h1>
+            <p className='subtitle'>
               Tracking my progress through the
-              <a href="https://100dayscss.com/">"100 Days of CSS" challenge</a>. A fun set of
-              exercises designed to level up you UI building game!
+              <a href='https://100dayscss.com/'>"100 Days of CSS" challenge</a>.
+              A fun set of exercises designed to level up you UI building game!
             </p>
           </div>
-          <div className="header-column">
-            <img className="header-image" src={CodeImage} alt="code" />
+          <div className='hero'>
+            <img className="banner-image" src={CodeImage} alt='code' />
           </div>
         </header>
-        <main className="main">
-          <ul className="progress-list">
+        <main className='content'>
+          <ul className='progress-list'>
             {pens.map((submission, index) => (
               <li
                 key={index}
                 className={cn({
-                  'active': activePen && submission?.id === activePen?.id,
+                  'active': activePen && (submission?.id === activePen?.id),
                   'completed': submission,
-                  'progress-item': true,
+                  'progress-item': true
                 })}
                 onClick={() => {
                   if (submission) setActivePen(submission);
-                }}>
+                }}
+              >
                 {index + 1}
               </li>
             ))}
           </ul>
-
-          <div className="reset">
-            <button className="reset-button" onClick={() => setActivePen(null)}>
+          <div className='reset'>
+            <button onClick={() => setActivePen(null)}>
               Reset Filter
             </button>
           </div>
-
           {activePen ? (
-            <IFrame title={activePen.title} id={activePen.id} classNames={{ active: true }} />
+            <IFrame
+              className='active'
+              title={activePen.title}
+              id={activePen.id}
+            />
           ) : (
-            <ul className="pen-list">
+            <ul className='pen-list'>
               {pens.filter(Boolean).map((s, index) => (
-                <IFrame key={index} title={s.title} id={s.id} />
+                <li key={index}>
+                  <IFrame title={s.title} id={s.id} />
+                </li>
               ))}
             </ul>
           )}
         </main>
       </div>
-    </div >
+    </div>
   );
 }
 
